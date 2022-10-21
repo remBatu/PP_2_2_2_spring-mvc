ccf1f6de8f04c0d1a3dae47f3117231e8cb6fd67
Владимир Хубецов <shepgrade@gamail.com>
2022-10-21 10:45:34 +0300

    done

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 public  class CarServiceImpl implements CarService {
 
 
-    private  CarDao carDao;
+    private final CarDao carDao;
     @Autowired
     public CarServiceImpl(CarDao carDao) {
         this.carDao = carDao;
